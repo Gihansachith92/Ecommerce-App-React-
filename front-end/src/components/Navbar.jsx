909d1468b7fe51bbcff2f5d4f3c67690afd6cd6e
Gihan_Sachith <gihansachith92@gmail.com>
2024-09-01 22:11:42 +0530

    hidden menu

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -42,7 +42,9 @@ function Navbar() {
 
         <Link to='/cart' className='relative'>
            <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
+           <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
         </Link>
+        <img src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
       </div>
 
     </div>
