6929e64caaa4435b61da1fb1ba9e93d30c7c6154
Gihan_Sachith <gihansachith92@gmail.com>
2024-09-19 18:25:24 +0530

    latest collection description

--- front-end/src/components/LatestCollection.jsx
+++ front-end/src/components/LatestCollection.jsx
@@ -13,7 +13,9 @@ const LatestCollection = () => {
     <div className='my-10'> 
       <div className='text-center py-8 text-3xl'>
          <Title text1={'LATEST'} text2={'COLLECTIONS'} />
-         <p className=''></p>
+         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores numquam perferendis dolore recusandae alias ad.
+         </p>
       </div>
     </div>
   )
