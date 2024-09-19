61e25feebfe53689306df1113f570f8cbbfe6275
Gihan_Sachith <gihansachith92@gmail.com>
2024-09-19 19:01:29 +0530

    complete latest product section

--- front-end/src/components/LatestCollection.jsx
+++ front-end/src/components/LatestCollection.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
 
@@ -19,6 +20,16 @@ const LatestCollection = () => {
            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores numquam perferendis dolore recusandae alias ad.
          </p>
       </div>
+
+     {/* Rendering products */}
+     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {
+          latestProducts.map((item,index)=>(
+            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+          ))
+        }
+     </div>
+
     </div>
   )
 }
