a2e5d412b567a6b88d3e4aeaa5ff3e5d312b7dcb
Gihan_Sachith <gihansachith92@gmail.com>
2024-12-29 12:13:56 +0530

    product data section

--- front-end/src/pages/Product.jsx
+++ front-end/src/pages/Product.jsx
@@ -7,8 +7,17 @@ const Product = () => {
  const {productId} = useParams();
  const {products} = useContext(ShopContext);
  const [productData,setProductData] = useState(false);
+ const [image,setImage] = useState('');
 
  const fetchProductData = async ()=> {
+
+  products.map((item)=>{
+     if (item._id === productId) {
+      setProductData(item);
+      setImage(item.image[0]);
+      return null;
+     }
+  })
 
  }
 
@@ -17,11 +26,17 @@ const Product = () => {
  },[productId,products])
 
 
-  return (
-    <div>
+  return productData ? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/* Product Data */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+         
+         {/* Product images */}
+
+      </div>
       
     </div>
-  )
+  ) : <div className='opacity-0'></div>
 }
 
 export default Product
