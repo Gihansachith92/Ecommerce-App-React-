00ee5883d77732ce116e47497229107214305176
Gihan_Sachith <gihansachith92@gmail.com>
2024-12-30 13:22:19 +0530

    complete related product section

--- front-end/src/pages/Product.jsx
+++ front-end/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
 import {ShopContext} from '../context/ShopContext'
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
 
@@ -89,6 +90,9 @@ const Product = () => {
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis, maxime ipsam eligendi quod nemo aliquid in cumque optio sed voluptatem molestiae! Quas ut quaerat dignissimos. Deleniti delectus similique odio unde.</p>
          </div>
       </div>
+
+      {/* ------Display related products--------- */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
       
     </div>
   ) : <div className='opacity-0'></div>
