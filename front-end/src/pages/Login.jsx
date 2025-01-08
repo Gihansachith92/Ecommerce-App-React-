91be4481e871958964e149581003a527f1ea507a
Gihan_Sachith <gihansachith92@gmail.com>
2025-01-08 12:18:04 +0530

    login form structure

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+ const [currentState,setCurrentState] = useState('Sign Up');
+
   return (
-    <div>
-      
-    </div>
+    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+      <div className=''>
+
+      </div>
+    </form>
   )
 }
 
